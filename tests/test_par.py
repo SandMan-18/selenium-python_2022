b49761ff86cd4c3a66d354546e9190bd1b392187
Manuel Tapia <romerotapiaj@gmail.com>
2022-04-26 21:04:09 -0500

    Ejercicios de clase 2 selenium-python

---
+++ tests/test_par.py
@@ -0,0 +1,26 @@
+from unittest import result
+
+
+def es_par(a, b):
+    if a % 2 == 0 and b % 2 == 0:
+        return True
+    else:
+        return False
+
+def test_positive():
+    result=es_par(2,4)
+    assert result==True, 'Los numeros son positivos'
+    
+    
+def test_negative():
+    result=es_par(3,9)
+    assert result==False, 'Los numeros no son positivos'
+    
+# def test_primer_numero_par():
+#     a=2
+#     b=4
+#     if a%2==0:
+#         es_par(a,b)
+#         assert result
+#     elif b%2==0:
+#         es_par(a,b)    
